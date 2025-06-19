b1da91385fb3164f5ba2130641fc544bb0e0e4f9
leandrolara <f609858@corp.caixa.gov.br>
2025-06-19 18:49:28 -0300

    feat(project): module gradle update

---
+++ features/details/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+apply("$rootDir/plugins/android-build.gradle")
+
+android {
+    namespace = "${Config.NAMESPACE}.features.details"
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // Modules
+    implementation(project(Modules.arch))
+    //implementation(project(Modules.domain))
+    //implementation(project(Modules.remote))
+    //implementation(project(Modules.navigation))
+    implementation(project(Modules.design))
+
+    // Libs
+
+    // TestLibs
+    testImplementation(testFixtures(project(Modules.testing)))
+    androidTestImplementation(testFixtures(project(Modules.testing)))
+    testImplementation(libs.okhttp)
+    testImplementation(libs.logging.interceptor)
+}
